448c90d994db21428388078aacaa31c8f62010c6
DIRECTOR228PLAY <150294748+DIRECTOR228PLAY@users.noreply.github.com>
2023-11-14 14:01:18 +0300

    Add files via upload

---
+++ задание 14.11 №2.cpp
@@ -0,0 +1,32 @@
+﻿// задание 14.11 №2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+
+using namespace std;
+
+int main()
+{
+    setlocale(0, "");
+    int a = 10;
+
+    int fibo1 = 0;
+    int fibo2 = 1;
+
+    cout << fibo1 << " ";
+    cout << fibo2 << " ";
+
+    for (int i = 2; i < a; ++i) {
+        int fibo = fibo1 + fibo2;
+
+        cout << fibo << " ";
+
+
+        fibo1 = fibo2;
+        fibo2 = fibo;
+    }
+
+    return 0;
+}
+
